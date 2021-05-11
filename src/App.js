800b8c97ec421503ae1e777b79fa4b9168f1e2fb
John J Davidson <xuz99@live.co.uk>
2021-05-11 19:02:27 +0100

    quick layout of inputs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import Form from './Form'
-import Button from '@material-ui/core/Button'
+
 
 function App() {
   return (
-    <>
     <Form />
-    <Button variant="contained" color="secondary">Hello</Button>
-    </>
   );
 }
 
--- src/Form.js
+++ src/Form.js
@@ -1,30 +1,119 @@
 import React from 'react';
+import HOSlogo from './hoslogo.svg';
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import '@fontsource/roboto';
 
 const Form = () => {
     return (    
-        <div className="App">
-            <section className=" ">
-            <h1 className="">HOS 007 Discount Tracker</h1>
-            <form action="#" method="POST">
-                <label htmlFor="first_name" className="">
-                First name
-                </label>
-                <input
-                type="text"
-                name="first_name"
-                id="first_name"
-                autoComplete="given-name"
-                className=""
+    <Container maxWidth="sm">
+        <section className="App">
+        <img src={HOSlogo} alt="Heritage of Scotland Logo" width="300" height="150"></img>
+        <Typography
+        variant="h3"
+        className=""
+
+        >HOS 007 Staff Discount Tracker</Typography>
+        <form className="" noValidate autoComplete="off">
+            <TextField 
+                id="standard-basic" 
+                label="Barcode*" 
+                inputProps="required"
+                type="number"
+                fullWidth
                 />
-                <button
-                type="submit"
-                className=""
-                >
-                Save
-                </button>
-            </form>
-            </section>
-        </div>
+            <TextField 
+                id="standard-basic" 
+                label="Date*" 
+                inputProps="required"
+                fullWidth
+                type="date"
+                />
+            <TextField 
+                id="standard-basic" 
+                label="Time of Sale *" 
+                inputProps="required"
+                fullWidth
+                type="time"
+                />
+            <TextField 
+                id="standard-basic" 
+                label="Staff who bought the items*" 
+                inputProps="required"
+                fullWidth
+                type="text"
+                />
+            <TextField 
+                id="standard-basic" 
+                label="Till Operator *" 
+                inputProps="required"
+                fullWidth
+                type="text"
+                />
+            <TextField 
+                id="standard-basic" 
+                label="Discount Type *" 
+                inputProps="required"
+                fullWidth
+                type="text"
+                />
+            <TextField 
+                id="standard-basic" 
+                label="Discount Percent (%) *" 
+                InputProps={{
+                    startAdornment: <InputAdornment position="start">%</InputAdornment>,
+                  }}
+                fullWidth
+                type="text"
+                />
+            <TextField 
+                id="standard-basic" 
+                label="Discounted Value *" 
+                InputProps={{
+                    startAdornment: <InputAdornment position="start">£</InputAdornment>,
+                  }}
+                fullWidth
+                type="text"
+                />
+            <TextField 
+                id="standard-basic" 
+                label="Actual Price *" 
+                InputProps={{
+                    startAdornment: <InputAdornment position="start">£</InputAdornment>,
+                  }}
+                fullWidth
+                type="text"
+                />
+            <TextField 
+                id="standard-basic" 
+                label="Final Value *" 
+                InputProps={{
+                    startAdornment: <InputAdornment position="start">£</InputAdornment>,
+                  }}
+                fullWidth
+                type="text"
+                />
+             <TextField 
+                id="standard-basic" 
+                label="Comments" 
+                fullWidth
+                type="text"
+                />
+            <hr/>
+            <Button 
+            size="large"
+            href="#" 
+            onClick={() =>alert('hello')} 
+            variant="contained" 
+            color="primary"
+            type="submit"
+            >Submit Discount</Button>
+        </form>
+        </section>
+    </Container>
     )
 }
 
