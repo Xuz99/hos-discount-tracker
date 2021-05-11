6c4a275927c912017568f3443fdb55f199edf886
John J Davidson <xuz99@live.co.uk>
2021-05-11 15:17:29 +0100

    installed material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Form from './Form'
+import Button from '@material-ui/core/Button'
 
 function App() {
   return (
+    <>
     <Form />
+    <Button variant="contained" color="secondary">Hello</Button>
+    </>
   );
 }
 
